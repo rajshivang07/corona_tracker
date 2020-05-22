2a6ed045d331e68b7b99063bf61d5f43ddec75a6
rajshivang07 <rajshivang07@gmail.com>
2020-05-22 21:28:46 +0530

    Updates

---
+++ precache-manifest.422ad9b4ac07a85489319a4f442d0ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2dd8626d55f010c370e191334d5411",
+    "url": "/corona_tracker/index.html"
+  },
+  {
+    "revision": "038207fbcf8d411358d7",
+    "url": "/corona_tracker/static/css/main.24e825e6.chunk.css"
+  },
+  {
+    "revision": "78e8876411aa23f356f6",
+    "url": "/corona_tracker/static/js/2.f1f7c360.chunk.js"
+  },
+  {
+    "revision": "9e39974f3e6ac20506d4134648d584fd",
+    "url": "/corona_tracker/static/js/2.f1f7c360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038207fbcf8d411358d7",
+    "url": "/corona_tracker/static/js/main.27d14467.chunk.js"
+  },
+  {
+    "revision": "4445a392914afdddce47",
+    "url": "/corona_tracker/static/js/runtime-main.a0cedbb3.js"
+  },
+  {
+    "revision": "3fdb664f4542233cefcd4d630f074210",
+    "url": "/corona_tracker/static/media/images.3fdb664f.jpeg"
+  }
+]);
